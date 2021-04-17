38f46429571e929cd7c0205d81096dce2695094b
afilerio <alfredofilerio@gmail.com>
2021-04-17 00:41:33 -0500

    added useState to change text on button depending on if button has been pressed or not, and added recording var to update firebase recording value

--- app/src/components/Record.js
+++ app/src/components/Record.js
@@ -1,17 +1,34 @@
 import { Button } from 'react-bootstrap'
-import React from 'react'
+import React, { useState } from 'react'
 
+let recording = false;
 
 // will contain button that user presses once to active recording feature, and can press once more when recording is over
 
 export function Record() {
+
+
+    const [buttonText, setButtonText] = useState('Start Recording!');
+
     const recordFunc = () => {
-        console.log("Hey fellas you're in the recordFun");
+        // console.log("Hey fellas you're in the recordFun");
+        console.log(recording);
+        if(recording){
+            setButtonText("Start Recording!");
+
+        }
+        else{
+            setButtonText("Stop Recording");
+        }
+        recording = !recording;
     };
+
     return (
         <div>
-            <Button onClick={recordFunc} >Start Recoding</Button>
+            <Button onClick={recordFunc} >{buttonText}</Button>
 
         </div>
     )
 }
+
+export {recording};
