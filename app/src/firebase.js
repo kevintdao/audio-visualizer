1ee0745288c4832bee3c153dc7de6bf1dbb172bd
afilerio <alfredofilerio@gmail.com>
2021-04-16 21:14:05 -0500

    moved firebase to app.js, added authentication

--- app/src/firebase.js
+++ app/src/firebase.js
@@ -2,14 +2,24 @@ import firebase from 'firebase/app';
 import "firebase/auth";
 import 'firebase/firestore';
 
-const app = firebase.initializeApp({
-    apiKey: "AIzaSyAnw_mndqxXa04-KxV8F_8SvOsK9Whqsx4",
-    authDomain: "audio-visualizer-e21e3.firebaseapp.com",
-    projectId: "audio-visualizer-e21e3",
-    storageBucket: "audio-visualizer-e21e3.appspot.com",
-    messagingSenderId: "493208962038",
-    appId: "1:493208962038:web:d892aec8668acb4157bdd7"
-  });
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-  export const auth = app.auth();
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyDjQu841eOqLzgyJF47A5nosb74qd5srZY",
+    authDomain: "audiovis-271a3.firebaseapp.com",
+    projectId: "audiovis-271a3",
+    storageBucket: "audiovis-271a3.appspot.com",
+    messagingSenderId: "1019309836920",
+    appId: "1:1019309836920:web:8e5d20f1c3a2f78c8ffb8d"
+  });
+  }
+  else{
+    firebase.app();
+  }
+  const auth = firebase.auth();
+  const firestore = firebase.firestore();
+  console.log("hello");
   export default app;
