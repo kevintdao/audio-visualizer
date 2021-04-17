80080794828c1ce361b420d022abd2cbeb775263
afilerio <alfredofilerio@gmail.com>
2021-04-17 00:13:26 -0500

    started recordFunc to not have it trigger when page loads

--- app/src/components/Record.js
+++ app/src/components/Record.js
@@ -5,9 +5,12 @@ import React from 'react'
 // will contain button that user presses once to active recording feature, and can press once more when recording is over
 
 export function Record() {
+    const recordFunc = () => {
+        console.log("Hey fellas you're in the recordFun");
+    };
     return (
         <div>
-            <Button>Start Recoding</Button>
+            <Button onClick={recordFunc} >Start Recoding</Button>
 
         </div>
     )
